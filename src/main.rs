071c994e04ba8587ab02c41957fd98b080e7237c
Rishabh Singh <rs4872962@gmail.com>
2026-04-13 21:06:24 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ use std::{
 };
 
 fn handle_stream(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
+    let mut buf =Vec::new();
 
     loop {
         match stream.read(&mut buf) {
-            Ok(_) => {
-
-                let res=String::from_utf8_lossy(&buf[..]);
-
-              
-              println!("{:?}",res.to_string());
-
+            Ok(res) => {
+            
+            println!("==> {:?}",String::from_utf8(buf.clone()));
+               
                 stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {

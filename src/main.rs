d27ba3de3bbab6213b6bb1ca5adb6ae668fe49c8
Rishabh Singh <rs4872962@gmail.com>
2026-04-13 10:09:05 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 #![allow(unused_imports)]
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     thread,
     time::Duration,
 };
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(_) => {
+                println!("{:?}", String::from_utf8_lossy(&buf[..]));
+
+                stream.write_all(b"+PONG\r\n").unwrap();
+            }
+            Err(e) => {
+                println!("{e}");
+                stream.write_all(b"+EEEror\r\n").unwrap();
+                break;
+            }
+        }
+        // buf.flush().unwrap();
+    }
+
+    // println!("accepted new connection");
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,32 +37,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf =[0;1024];
-
-                loop {
-                    match stream.read(&mut buf) {
-                        Ok(_) => {
-
-                            // println!("{:?}",String::from_utf8_lossy(&buf[..]));
-
-                            stream.write_all(b"+PONG\r\n").unwrap();
-                        }
-                        Err(e) => {
-                            println!("{e}");
-                            stream.write_all(b"+EEEror\r\n").unwrap();
-                            break;
-                        }
-                    }
-                    // buf.flush().unwrap();
-                }
-
-                // println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        if let Ok(stream) = stream {
+            thread::spawn(|| handle_stream(stream));
         }
     }
 }

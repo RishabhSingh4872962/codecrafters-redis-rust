92aac512970d0597965d6a95ab59fb7c877ef1c8
Rishabh Singh <rs4872962@gmail.com>
2026-04-13 08:26:55 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment the code below to pass the first stage
-    
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buf = Vec::new();
+                // let res=   stream.peek(&mut buf).expect("Byte see");
 
-                  stream.write_all(b"+PONG\r\n").unwrap();
-               let mut buf =Vec::new();
-            // let res=   stream.peek(&mut buf).expect("Byte see");
+                let res = stream.read_to_end(&mut buf).unwrap();
 
-            let res=stream.read_to_end(&mut buf).unwrap();
+                match String::from_utf8(buf) {
+                    Ok(str) => {
+                        if str.as_str() == "PING" {
+                            stream.write_all(b"+PONG\r\n").unwrap();
+                        }
+                    }
+                    Err(_e) => {}
+                }
 
-                println!("{:?}  ,res==> {:?}",stream,String::from_utf8(buf));
+                // println!("{:?}  ,res==> {:?}",stream,String::from_utf8(buf));
 
-
-
-
-              
                 // println!("accepted new connection");
             }
             Err(e) => {

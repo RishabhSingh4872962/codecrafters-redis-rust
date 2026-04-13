3cebbc5dba0191bd8aee0ae25ed235252b446af7
Rishabh Singh <rs4872962@gmail.com>
2026-04-13 23:57:21 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,30 @@ use std::{
 };
 
 fn handle_stream(mut stream: TcpStream) {
-    let mut buf = Vec::new();
+    let mut buf = [0; 1024];
 
     loop {
         match stream.read(&mut buf) {
-            Ok(res) => {
+            Ok(_res) => {
+                let str = String::from_utf8_lossy(&buf[..]);
 
-                let v=buf.clone();
-            
-            println!("==> {:?}",String::from_utf8(v));
-               
-                stream.write_all(b"+PONG\r\n").unwrap();
+                let str: String = str.split("\r\n").collect();
+
+                let res = handle_stream_parser(&str);
+
+                println!("ress===> {:?}",res);
+
+                match res[0] {
+                    "PING" => {
+                        stream.write_all(b"+PONG\n\r").unwrap();
+                    }
+                    "ECHO" => {
+                        let s = format!("${}\r\n{}\r\n", res[1].len(), res[1]);
+
+                        stream.write_all(s.as_bytes()).unwrap();
+                    }
+                    _ => {}
+                }
             }
             Err(e) => {
                 println!("{e}");
@@ -31,9 +44,46 @@ fn handle_stream(mut stream: TcpStream) {
     // println!("accepted new connection");
 }
 
+fn handle_stream_parser(str: &str) -> Vec<&str> {
+    let first_char = &str[..1];
 
-fn handle_stream_parser(){
+    let mut v: Vec<&str> = Vec::new();
 
+    match first_char {
+        "*" => {
+            let arr_length: u8 = str[1..2].parse().unwrap();
+
+            handle_ele_parse(&str[2..], &mut v, arr_length);
+        }
+        _ => {}
+    }
+
+    v
+}
+
+fn handle_ele_parse<'a>(str: &'a str, v: &mut Vec<&'a str>, n: u8) {
+    let first_char = &str[..1];
+
+    match first_char {
+        "$" => {
+            let str_length: usize = str[1..2].parse().unwrap();
+
+            let start: usize = 2;
+
+            let end: usize = 2 + str_length;
+
+            let s: &str = &str[start..end];
+
+            v.push(s);
+
+            if n == 1 {
+                return;
+            }
+
+            handle_ele_parse(&str[end..], v, n - 1)
+        }
+        _ => {}
+    }
 }
 
 fn main() {
@@ -49,4 +99,6 @@ fn main() {
             thread::spawn(|| handle_stream(stream));
         }
     }
+
+    // let req = b"*1\r\n$4\r\nPING\r\n";
 }

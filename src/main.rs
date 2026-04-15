98cb7b8ef280bd077caabff72045893cbe5a7863
Rishabh Singh <rs4872962@gmail.com>
2026-04-15 12:27:05 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn handle_stream(mut stream: TcpStream, mut store: HashMap<String, Value>) {
 
                 // let uppper_str = str.to_uppercase();
 
-                let res = parser(&str);
+                let res =[];
 
                 // println!("ress===> {:?}", res);
 

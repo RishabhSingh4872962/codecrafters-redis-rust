3f5cb7326ffcc917e8a6ee04b1adf28eb8f2eb3c
Rishabh Singh <rs4872962@gmail.com>
2026-04-18 22:38:33 +0530

    codecrafters submit [skip ci]

--- src/commands/lpop.rs
+++ src/commands/lpop.rs
@@ -16,18 +16,49 @@ pub fn handle_lpop(
 ) {
     let key = res[1];
 
+    let removed_ele_count = res.get(2).and_then(|e| e.parse::<usize>().ok());
+
     if let Some(val) = list_store.get_mut(key) {
-        let first_ele = val.value.pop_front();
+        if let Some(count) = removed_ele_count {
+            let response = handle_pop_to_string(&mut val.value, count);
 
-        if let Some(ele) = first_ele {
-            let s = create_string_response(&ele);
-
-            stream.write_all(s.as_bytes()).unwrap();
-            
+            stream.write_all(response.as_bytes()).unwrap();
         } else {
-            stream.write_all(NULL_BULK_STRING.as_bytes()).unwrap();
+            let first_ele = val.value.pop_front();
+
+            if let Some(ele) = first_ele {
+                let s = create_string_response(&ele);
+
+                stream.write_all(s.as_bytes()).unwrap();
+            } else {
+                stream.write_all(NULL_BULK_STRING.as_bytes()).unwrap();
+            }
         }
     } else {
         stream.write_all(NULL_BULK_STRING.as_bytes()).unwrap();
     }
+}
+
+
+
+fn handle_pop_to_string(val: &mut VecDeque<String>, mut n: usize) -> String {
+    n = if n > val.len() { val.len() } else { n };
+
+    let mut res = format!("*{}\r\n", n);
+
+    loop {
+        if n == 0 {
+            break;
+        }
+        if let Some(poped_ele) = val.pop_front() {
+            let s = create_string_response(&poped_ele);
+
+            res.push_str(&s);
+        } else {
+            break;
+        }
+        n -= 1;
+    }
+
+    res
 }
